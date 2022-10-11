ae4b27557347a7506178f0d3b9aaff1e47bcc19b
yurassic <ynyay@assurance.com>
2022-10-12 00:32:24 +0300

    Update mock data

--- product-service/handlers/getProductsById.js
+++ product-service/handlers/getProductsById.js
@@ -5,6 +5,9 @@ export const handler = async (event) => {
   const product = products.find(item => item.id === productId)
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
     body: JSON.stringify(product),
   }
 }
--- product-service/handlers/getProductsList.js
+++ product-service/handlers/getProductsList.js
@@ -3,6 +3,9 @@ import products from '../__mocks__/products.json'
 export const handler = async () => {
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
     body: JSON.stringify(products)
   }
 }
