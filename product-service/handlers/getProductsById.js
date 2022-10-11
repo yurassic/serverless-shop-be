e1b198bb52495e1b1867ee437ac72e7b2ef50ce2
yurassic <ynyay@assurance.com>
2022-10-12 00:58:06 +0300

    Handle not found for productById

--- product-service/handlers/getProductsById.js
+++ product-service/handlers/getProductsById.js
@@ -3,6 +3,15 @@ import products from '../__mocks__/products.json'
 export const handler = async (event) => {
   const { productId } = event.pathParameters
   const product = products.find(item => item.id === productId)
+  if (!product) {
+    return {
+      statusCode: 404,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: 'Product not found',
+    }
+  }
   return {
     statusCode: 200,
     headers: {
