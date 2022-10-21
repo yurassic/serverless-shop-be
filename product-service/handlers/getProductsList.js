ec86ca1ea8e4057e6096df6e9aefcb89248c1469
yurassic <ynyay@assurance.com>
2022-10-21 15:35:01 +0300

    Add dynamoDB for products list

--- product-service/handlers/getProductsList.js
+++ product-service/handlers/getProductsList.js
@@ -1,8 +1,43 @@
-import products from '../__mocks__/products.json'
+import AWS from 'aws-sdk';
 
-export const handler = async () => {
+const dynamoDB = new AWS.DynamoDB.DocumentClient()
+
+const scan = async (params, callback) => {
+  try {
+    const data = await dynamoDB.scan(params).promise()
+    return data.Items;
+  } catch (error) {
+    console.error(error)
+    callback(null, {
+      statusCode: error.statusCode || 501,
+      headers: { 'Content-Type': 'text/plain' },
+      body: `Couldn\'t fetch the ${params.TableName}.`,
+    })
+  }
+}
+
+const productTableParams = {
+  TableName: process.env.DYNAMODB_PRODUCTS_TABLE
+}
+
+const stockTableParams = {
+  TableName: process.env.DYNAMODB_STOCKS_TABLE
+}
+
+export const handler = async (event, context, callback) => {
+  console.log(event, 'Lambda request')
+
+  const products = await scan(productTableParams, callback)
+  const stocks = await scan(stockTableParams, callback)
+
+  const result = products.map(product => {
+    const relatedStock = stocks.find(stock => product.id === stock.product_id)
+    // SEE: I decide to return 0 if no data in stocks, for me it's looks more clear than don't have count in response. 
+    return {...product, count: relatedStock ? relatedStock.count : 0}
+  })
+
   return {
     statusCode: 200,
-    body: JSON.stringify(products)
+    body: JSON.stringify(result)
   }
 }
